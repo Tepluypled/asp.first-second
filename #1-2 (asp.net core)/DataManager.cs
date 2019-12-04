31174ccba31ad86132fe5835a58ec443a4c53ef0
TepluyPled <tepluypled@gmail.com>
2019-12-04 22:17:56 +0200

    first commit

---
+++ #1-2 (asp.net core)/DataManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2
+{
+    public class DataManager:IDataManager
+    {
+        public int[] GenerateNumber()
+        {
+            int[] numbers = new int[100];
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                numbers[i] = random.Next(100);
+            }
+            return numbers;
+        }
+
+    }
+}
